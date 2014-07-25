55a33d2b6e6ef3e3bdfccbab6a55b6f1437f0f90
mfw <mfw@mfwtekiMacBook-Pro.local>
2014-07-25 19:10:54 +0800

    master测试用例

---
+++ Master.m
@@ -0,0 +1,42 @@
+//
+//  Master.m
+//  LocalPush_Test
+//
+//  Created by mfw on 14-7-25.
+//  Copyright (c) 2014年 MFW. All rights reserved.
+//
+
+#import "Master.h"
+
+@interface Master ()
+
+@end
+
+@implementation Master
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    /**
+     *  测试
+     *
+     *  @return 测试用例
+     */
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
